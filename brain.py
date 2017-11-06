d0c072d3740fc19b8f61772c350164f681ea1f91
Rob Deans <robertdeans@ntlworld.com>
2017-11-06 17:32:35 +0000

    Updated the methods

--- brain.py
+++ brain.py
@@ -1,12 +1,12 @@
-# This is the main file where we train and test the models
+# This is the main file where we train and test the ys
 import pandas as pd
 import tensorflow as tf
 import numpy as np
 
-# Config options for the model etc
+# Config options for the y etc
 TRAIN_SIZE = 0.6
 
-HIDDEN_NEURONS = 12
+HIDDEN_NEURONS = 3
 
 # Read in the csv
 df = pd.read_csv('norm_iris.csv')
@@ -20,9 +20,9 @@ output = [[row['setosa'], row['versicolor'], row['virginica']] for _, row in df.
 NUM_INPUTS = len(petal_input[0])
 NUM_OUTPUTS = len(output[0])
 
-# Training input is what we are going to train the model on
+# Training input is what we are going to train the y on
 training_input = petal_input[:int(len(petal_input) * TRAIN_SIZE)]
-# Test input is what we we use to test how well the model performs
+# Test input is what we we use to test how well the y performs
 test_input = petal_input[int(len(petal_input) * TRAIN_SIZE):]
 
 # Likewise we will split the expected output in the same way
@@ -33,24 +33,21 @@ input_data = training_input
 output_data = training_output
 
 # Set up the tf placeholders
-input_x = tf.placeholder(dtype=np.float32, shape=[None, NUM_INPUTS], name='input_x')
-output_x = tf.placeholder(dtype=np.float32, shape=[None, NUM_OUTPUTS], name='output_x')
+x = tf.placeholder(dtype=np.float32, shape=[None, NUM_INPUTS], name='x')
+y_ = tf.placeholder(dtype=np.float32, shape=[None, NUM_OUTPUTS], name='y_')
 
 # Setting up the hidden layers:
-hidden_W = tf.Variable(tf.random_normal([NUM_INPUTS, HIDDEN_NEURONS]))
-hidden_b = tf.Variable(tf.random_normal([HIDDEN_NEURONS]))
+W = tf.Variable(tf.zeros([NUM_INPUTS, HIDDEN_NEURONS]))
+b = tf.Variable(tf.zeros([HIDDEN_NEURONS]))
 
-# Calculate the hidden layer values
-hidden = tf.sigmoid(tf.matmul(input_x, hidden_W) + hidden_b)
-
-# Calculate the output
-output_W = tf.Variable(tf.random_normal([HIDDEN_NEURONS, NUM_OUTPUTS]))
-output = tf.sigmoid(tf.matmul(hidden, output_W))
+y = tf.nn.softmax(tf.matmul(x, W) + b)  # Soft max
 
 # Loss function:
-cost = tf.reduce_mean(tf.square(output_x - output))
-optimiser = tf.train.AdamOptimizer(0.01)
-train = optimiser.minimize(cost)
+cost_function = -tf.reduce_sum(y_ * tf.log(y))
+optimiser = tf.train.AdamOptimizer(0.01).minimize(cost_function)
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # Init all the tf variables etc
 init = tf.global_variables_initializer()
@@ -60,23 +57,16 @@ sess.run(init)
 
 # Train the data:
 
-for i in range(10001):
-    cvalues = sess.run([train, cost, hidden_W, output_x], feed_dict={input_x: input_data, output_x: output_data})
+data = []
+data_2 = []
+
+for i in range(5000):
+    cvalues = sess.run([optimiser, cost_function], feed_dict={x: training_input, y_: training_output})
+
+    if i % 100:
+        print(cvalues[1])
 
 correct = 0
 
-for j, val in enumerate(test_input):
-
-    conf = sess.run(output, feed_dict={input_x: [val]}).tolist()
-    guess = conf[0].index(max(conf[0]))
-    correct_output = test_output[j].index(max(test_output[j]))
-
-    if guess == correct_output:
-        print('Guess: {} | Correct output: {}'.format(guess, correct_output))
-        print("")
-        correct = correct + 1
-    else:
-        print('Guess: {} | Correct output: {}'.format(guess, correct_output))
-        print("")
-
-print(correct/len(test_input))
+print("")
+print(sess.run(accuracy, feed_dict={x: test_input, y_: test_output}))
